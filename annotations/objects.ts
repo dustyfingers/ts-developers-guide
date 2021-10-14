fc3606d99f5bd13753db78c6e089148e34df5e8a
lou <dustyfingers01257@gmail.com>
2021-10-13 22:15:12 -0600

    learned about object destructuring in ts

---
+++ annotations/objects.ts
@@ -0,0 +1,16 @@
+const profile = {
+    name: 'alex',
+    age: 25,
+    coords: {
+        lat: 0,
+        lng: 22
+    },
+    setAge(age: number): void {
+        this.age = age;
+    }
+}
+
+// this is how to destructure from objects in ts
+const { age, name }: { age: number, name: string } = profile;
+
+const { coords: {lat, lng}}: {coords: { lat: number, lng: number }} = profile;
