6beaa0b5266fd4811889e26b7583e417d597c6e8
lou <dustyfingers01257@gmail.com>
2021-10-14 20:32:22 -0600

    learned about type guards in ts

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,16 +1,36 @@
 class Sorter {
   // shorter way of writing constructors
-  constructor(public collection: number[]) {}
+  constructor(public collection: number[] | string) {}
 
   sort(): void {
     const { length } = this.collection;
+
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j+1]) {
-          const temp = this.collection[j];
-          this.collection[j] = this.collection[j+1];
-          this.collection[j+1] = temp;
+
+        // if collection is an array of numbers
+        if (this.collection instanceof Array) {
+          if (this.collection[j] > this.collection[j+1]) {
+            const temp = this.collection[j];
+            this.collection[j] = this.collection[j+1];
+            this.collection[j+1] = temp;
+          }
         }
+
+        // if collection is a string
+        // this doubling is bad practice
+        // if (typeof this.collection === 'string') {
+        //   if (this.collection[j] > this.collection[j+1]) {
+        //     const temp = this.collection[j];
+        //     // this wont work with strings
+        //     this.collection[j] = this.collection[j+1];
+        //     this.collection[j+1] = temp;
+        //   }
+        // }
+
+        // typeof varName === 'string' etc is for primitives
+        // varName instanceof Array etc is for every other value created with a constructor function
+
       }
     }
   }
