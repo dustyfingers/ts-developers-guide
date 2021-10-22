1bc8e6fc90bcca451357c3bf1794bdffc3c85576
lou <dustyfingers01257@gmail.com>
2021-10-21 23:24:34 -0600

    stats project init

--- sort/src/Sorter.ts
+++ sort/src/Sorter.ts
@@ -5,15 +5,13 @@ export interface Sortable {
 }
 
 export class Sorter {
-  // shorter way of writing constructors
-  constructor(public collection: Sortable) {}
 
   sort(): void {
-    const { length } = this.collection;
+    const { length } = this;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection.compare(j)) this.collection.swap(j);
+        if (this.compare(j)) this.swap(j);
         // typeof varName === 'string' etc is for primitives
         // varName instanceof Array etc is for every other value created with a constructor function
       }
