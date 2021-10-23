61ba41dcea629e5b262d79d831832de24e1ea82b
lou <dustyfingers01257@gmail.com>
2021-10-23 08:34:13 -0600

    built football match parser

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,13 +1,17 @@
 import fs from 'fs';
 
 const matches = fs
-  .readFileSync('football.csv', {
-    encoding: 'utf-8'
-  })
-  .split('/n')
-  .map((row: string): string[] => {
-    return row.split(',')
-  })
+  .readFileSync('football.csv', { encoding: 'utf-8' })
+  .split('\n')
+  .map((row: string): string[] => row.split(','))
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if ((match[1] === 'Man United') && (match[5] === 'H')) manUnitedWins++;
+  else if ((match[2] === 'Man United') && (match[5] === 'A')) manUnitedWins++;
+}
 
 console.log(matches);
+console.log(manUnitedWins);
 
