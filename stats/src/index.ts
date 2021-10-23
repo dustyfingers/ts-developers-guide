3adb4c6844d41491ec610f41113b4c423101bf1f
lou <dustyfingers01257@gmail.com>
2021-10-23 08:45:22 -0600

    built match result enum

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -7,9 +7,20 @@ const matches = fs
 
 let manUnitedWins = 0;
 
+// match result enum
+// enums can only hold strings or numbers!
+// just a collection of closely related values
+// also creates a new type
+// eg you can create a function that returns type MatchResult now
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D'
+}
+
 for (let match of matches) {
-  if ((match[1] === 'Man United') && (match[5] === 'H')) manUnitedWins++;
-  else if ((match[2] === 'Man United') && (match[5] === 'A')) manUnitedWins++;
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) manUnitedWins++;
+  else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) manUnitedWins++;
 }
 
 console.log(matches);
