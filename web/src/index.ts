ed958f8b3d8ac0b2a955380ed415849a6516d9a8
lou <dustyfingers01257@gmail.com>
2021-10-30 09:17:22 -0600

    learned about optional interface properties and built set method

--- web/src/index.ts
+++ web/src/index.ts
@@ -4,3 +4,13 @@ const user = new User({ name: 'lou', age: 24 });
 
 console.log(user.get('name'));
 console.log(user.get('age'));
+
+// before optinal props 
+user.set({ name: 'newname', age: 99 });
+
+// after
+user.set({ name: 'newnametoo' });
+user.set({ age: 123 });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,6 +1,7 @@
 interface UserProps {
-  name: string; 
-  age: number;
+  // ? makes the prop optional
+  name?: string; 
+  age?: number;
 }
 
 export class User {
@@ -8,5 +9,9 @@ export class User {
 
   get(propName: string): (string | number) {
     return this.data[propName];
+  }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
   }
 }
